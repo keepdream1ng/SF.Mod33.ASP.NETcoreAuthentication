66804ec60589600b5dd552029cbb2d433c8dccf7
Anatolii Ivanov <gotnickname@gmail.com>
2023-10-14 12:00:41 +0530

    added custom exception as per module task

---
+++ SF.Mod33.ASP.NETcoreAuthentication.BLL/Exceprions/CustomException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF.Mod33.ASP.NETcoreAuthentication.BLL;
+
+public class CustomException : Exception
+{
+    public CustomException(string message): base(message)
+    {
+    }
+}
